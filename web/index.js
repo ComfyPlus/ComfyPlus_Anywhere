6115921939fb65d4390c2ac49e4564efc039d533
nomad <nomad@comfyplus.org>
2024-10-23 12:13:00 +0800

    update

--- web/index.js
+++ web/index.js
@@ -116,7 +116,7 @@ app.registerExtension({
                     else if(text == DISCONNECT_TEXT) {
                         let resp = await api.fetchApi("/comfyplus_anywhere/disconnect", {method: "POST", body: "{}"});
                         resp = await resp.json();
-                        this.check_status(resp);
+                        await this.check_status(resp);
                     }
                 }
             }),
